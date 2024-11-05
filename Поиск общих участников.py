84c77dc764a23ffa8f2f19ed65536454dba1af54
aleksandrgorshkov <39491089+aleksandrgorshkov@users.noreply.github.com>
2024-11-05 20:31:53 +0300

    Поиск общих участников.py

--- Поиск общих участников.py
+++ Поиск общих участников.py
@@ -4,17 +4,13 @@ def find_common_participants(group1, group2, n=','):
 	common_participants = first.intersection(second)
 	return sorted(common_participants)
 
-# Пример использования с разделителем '|'
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
-# Вызов функции без указания третьего аргумента, будет использован разделитель '|'
 common_participants = find_common_participants(participants_first_group, participants_second_group)
 print(common_participants)
 
-# Пример использования с другим разделителем (например, запятая)
 participants_first_group_comma = "Иванов,Петров,Сидоров"
 participants_second_group_comma = "Петров,Сидоров,Смирнов"
 
-# Вызов функции с указанием третьего аргумента, разделитель будет ','
 print (find_common_participants(participants_first_group_comma, participants_second_group_comma, n=','))
